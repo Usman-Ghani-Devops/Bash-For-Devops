c82ec8333934c0532ea36cf3645a51ba5dba34ce
Usman Ghani <usman328ghani@gmail.com>
2026-03-07 04:59:09 +0000

    Centralized_Log_Analyzer

--- Centralized_Log_Analyzer/Centralized_Log_Analyzer.sh
+++ Centralized_Log_Analyzer/Centralized_Log_Analyzer.sh
@@ -3,7 +3,7 @@ set -e
 shopt -s nullglob
 
 read -r -p "Enter the Location of your Log Folder: " location
-total=0
+total_logs=0
 files=0
 declare -A levels
 declare -A messages
@@ -15,11 +15,11 @@ do
 		while read -r line
 
 		do
-			level = $(echo "$line" | awk '{print 3}')
-			((total++))
+			level=$(echo "$line" | awk '{print 3}')
+			((total_logs++))
 
 			((levels["$level"]++))
-			message = $(echo "$line" | awk '{print 4}')
+			message=$(echo "$line" | awk '{print 4}')
 
 			if [[ "$level" == "Error" ]];then
 				((messages["$message"]++))
@@ -41,7 +41,22 @@ do
 		top="$err"
 	fi
 done
+{
 
-echo max_count
-echo top_error
+echo "---Log Analysis Report---"
 
+echo "Total Files Processed: $files"
+
+echo "Total Logs: $total_logs"
+
+
+for err in "${!levels[@]}"
+do
+	echo " $err " ${levels[$err]}"
+done
+echo "Most Common Error"
+echo "$top_error ($max_count) times"
+
+} > Report.txt
+
+echo "Report Generated"
